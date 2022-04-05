f668b558ced4721e7f9497e1526208b9c42f189a
mino89 <pierdomenicoreitano@gmail.com>
2022-04-05 02:04:06 +0200

    implemented keyup and change control

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ class Formaggino {
     this.version = LIB_VERSION;
   }
 
+  debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+      const later = () => {
+        clearTimeout(timeout);
+        func(...args);
+      };
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    };
+  }
+
   setupRequest({ method, body, headers }) {
     headers = { ...headers };
     if (!(body instanceof FormData)) {
@@ -49,51 +61,101 @@ class Formaggino {
     }
   }
 
-  validate(form, { errorClass, loadingClass, mode } = {}) {
+  checkInputError(e, classError) {
+    this.errorFinder(e.target, classError, e.target.checkValidity());
+  }
+
+  errorFinder(el, classError, valid) {
+    while (el) {
+      if (el.classList.contains(classError) && !valid) {
+        el.classList.add("active");
+      } else {
+        el.classList.remove("active");
+      }
+      el = el.nextElementSibling;
+    }
+  }
+
+  triggerReport(el, timing) {
+    document.querySelector(`.${el}`).classList.add("active");
+    setTimeout(() => {
+      document.querySelector(`.${el}`).classList.remove("active");
+    }, timing);
+  }
+
+  validate(form, { errorClass } = {}) {
     form.preventDefault();
+    console.log(form);
     const formEl = form.target;
-    const url = formEl.action;
-    const type = formEl.method;
-    let data = new FormData(formEl);
 
     // fields errors
     const classError = errorClass ? errorClass : "form-error";
     const list = formEl.querySelectorAll(":invalid");
     const validFields = formEl.querySelectorAll(".active");
 
-    // loading element
-    const loading = loadingClass ? loadingClass : "form-loading";
-    const loadingEl = document.querySelector(`.${loading}`);
-    const dataFormat = mode ? mode : "form-data";
-    console.log(classError);
-    if (dataFormat === "json") {
-      data = Object.fromEntries(data.entries());
-    }
     // reset for validation
     validFields.forEach((el) => el.classList.remove("active"));
 
+    formEl.addEventListener("change", (e) => {
+      this.checkInputError(e, errorClass);
+    });
+
+    formEl.addEventListener("keyup", (e) => {
+      this.debounce(this.checkInputError(e, errorClass), 400);
+    });
+
     if (formEl.checkValidity()) {
-      loadingEl.classList.add("active");
+      return true;
+    } else {
+      list.forEach((el) => {
+        this.errorFinder(el, classError, false);
+      });
+      return false;
+    }
+  }
+
+  submit(
+    form,
+    {
+      mode,
+      loadingClass,
+      errorClass,
+      formSuccess,
+      formError,
+      closingTiming,
+    } = {}
+  ) {
+    const formEl = form.target;
+    const url = formEl.action;
+    const type = formEl.method;
+    const dataFormat = mode ? mode : "form-data";
+    const loading = loadingClass ? loadingClass : "form-loading";
+    const reportError = formError ? formError : "form-report-error";
+    const reportSuccess = formSuccess ? formSuccess : "form-report-success";
+    const closeAfter = closingTiming ? closingTiming : 3000;
+    const loadingEl = document.querySelector(`.${loading}`);
+    let data = new FormData(formEl);
+
+    let isValid = this.validate(form, { errorClass });
+
+    if (dataFormat === "json") {
+      data = Object.fromEntries(data.entries());
+    }
+
+    if (isValid) {
       if (type === "post") {
+        loadingEl.classList.add("active");
         this.post(url, data)
           .then(this.checkError)
-          .then((response) => {
+          .then(() => {
+            this.triggerReport(reportSuccess, closeAfter);
             formEl.reset();
           })
-          .catch((error) => console.log(error, 'catch'))
+          .catch(() => this.triggerReport(reportError, closeAfter))
           .finally(() => {
             loadingEl.classList.remove("active");
           });
       }
-    } else {
-      list.forEach((el) => {
-        while (el) {
-          if (el.classList.contains(classError)) {
-            el.classList.add("active");
-          }
-          el = el.nextElementSibling;
-        }
-      });
     }
   }
 }
